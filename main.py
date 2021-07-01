ad6b103e80be85e0a98d4d38ba0076f2218bbfff
luboolu <kjjy98@naver.com>
2021-07-01 23:05:57 +0900

    210701

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+from collections import deque
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def dfs(graph, v, visited):
+    #현재 노드를 방문처리
+    visited[v] = True
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
+def bfs(graph, start, visited):
+    # 큐 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    #현재 노드를 방문 처리
+    visited[start] = True
+    #큐가 빌 때까지 반복
+    while queue:
+        #큐에서 하나의 원소를 뽑아 출력하기
+        v = queue.popleft()
+        print(v, end=' ')
+        #아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+visited = [False] * len(graph)
+dfs(graph, 1, visited)
+print("")
+visited = [False] * len(graph)
+bfs(graph, 1, visited)
