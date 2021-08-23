213d06e780df5b119726822497c606dd84577696
luboolu <kjjy98@naver.com>
2021-08-23 23:39:44 +0900

    210823

--- WeeklyChallenge/직업군추천하기/solution.py
+++ WeeklyChallenge/직업군추천하기/solution.py
@@ -1,29 +1,27 @@
 def solution(table, languages, preference):
     answer = ''
-    job = ['SI', 'CONTENTS', 'HARDWARE', 'PORTAL', 'GAME']
+    job = []
+    scores = []
 
-    if len(languages) == 1:
-        return languages[0]
-    else:
-        scores = []
+    for t in table:
+        split_table = t.split(" ")
+        job.append(split_table[0])
+        tmp_score = 0
 
-        for t in table:
-            split_table = t.split(" ")
-            tmp_score = 0
+        for i in range(len(languages)):
+            if languages[i] in split_table:
+                tmp_score += (6 - split_table.index(languages[i])) * preference[i]
 
-            for i in range(len(languages)):
-                if languages[i] in split_table:
-                    tmp_score += (6 - split_table.index(languages[i])) * preference[i]
+        scores.append(tmp_score)
 
-            scores.append(tmp_score)
 
-        max_score = max(scores)
-        jobs = []
+    max_score = max(scores)
+    jobs = []
 
-        while max_score in scores:
-            jobs.append(job[scores.index(max_score)])
-            scores[scores.index(max_score)] = 0
+    while max_score in scores:
+        jobs.append(job[scores.index(max_score)])
+        scores[scores.index(max_score)] = 0
 
-        jobs = sorted(jobs)
+    jobs = sorted(jobs)
 
-        return jobs[0]
+    return jobs[0]
