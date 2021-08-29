f94e7a3044c5c9e29d9ac065bfbf9ffdaf2acd9f
luboolu <kjjy98@naver.com>
2021-08-29 20:56:03 +0900

    210829

--- Level2/괄호변환/solution.py
+++ Level2/괄호변환/solution.py
@@ -1,9 +1,13 @@
 def solution(p):
     answer = ''
-
+    balence = 0
     if p == "":
         return answer
     else:
-        pass
+        for i in p:
+            if i == "(":
+                balence += 1
+            else:
+                balence -= 1
 
     return answer
