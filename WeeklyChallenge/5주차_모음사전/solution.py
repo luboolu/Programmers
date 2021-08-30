63b4493a90d1a6f82e7171c9f3893e95ba68e453
luboolu <kjjy98@naver.com>
2021-08-30 19:00:09 +0900

    210830

--- WeeklyChallenge/5주차_모음사전/solution.py
+++ WeeklyChallenge/5주차_모음사전/solution.py
@@ -1,4 +1,26 @@
 def solution(word):
     answer = 0
+    nums = ''
+    au = ["A", "E", "I", "O", "U"]
 
-    return answer
+    for w in word:
+        nums += str(au.index(w) + 1)
+
+    nums = int(nums)
+    notIn = ['6', '7', '8', '9', '0']
+    dic = []
+
+    for i in range(55556):
+        istr = str(i)
+        flag = True
+        for n in notIn:
+            if n in istr:
+                flag = False
+                break
+
+        if flag == True:
+            dic.append(istr)
+
+    dic = sorted(dic)
+
+    return dic.index(str(nums)) + 1
