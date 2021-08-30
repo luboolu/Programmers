2dfc65f2bad52f53b6e17a406c9544edf676abee
luboolu <kjjy98@naver.com>
2021-08-30 21:16:55 +0900

    210830

--- Level2/괄호변환/solution.py
+++ Level2/괄호변환/solution.py
@@ -1,13 +1,32 @@
-def solution(p):
+def solution(P):
     answer = ''
-    balence = 0
-    if p == "":
-        return answer
+
+    balence, i = isBalenced(P)
+
+    if balence == True:
+        return P
     else:
-        for i in p:
-            if i == "(":
-                balence += 1
-            else:
-                balence -= 1
+        u = P[:i]
+        v = P[i:]
+
+        print(u, "---", v)
 
     return answer
+
+
+def isBalenced(P):
+    balence = 0
+    isBalence = True
+
+    for i in range(len(P)):
+        if P[i] == "(":
+            balence += 1
+        else:
+            balence -= 1
+
+        if balence < 0:
+            isBalence = False
+            return isBalence, i
+
+    return isBalence, 0
+
