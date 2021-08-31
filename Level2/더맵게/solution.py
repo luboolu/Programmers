317408ab023b811d211bf3c47372e25bb2da2e10
luboolu <kjjy98@naver.com>
2021-08-31 13:19:58 +0900

    210831

---
+++ Level2/더맵게/solution.py
@@ -0,0 +1,21 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while True:
+        min = heapq.heappop(scoville)
+
+        if min >= K:
+            break
+        elif len(scoville) <= 1 and min < K:
+            answer = -1
+            break
+        else:
+            min_2 = heapq.heappop(scoville)
+            new_food = min + min_2 * 2
+            heapq.heappush(scoville, new_food)
+            answer += 1
+
+    return answer
