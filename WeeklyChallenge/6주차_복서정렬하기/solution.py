60d52f8d21304c31ae799698274be67e42a665c9
luboolu <kjjy98@naver.com>
2021-09-06 21:06:37 +0900

    210906

--- WeeklyChallenge/6주차_복서정렬하기/solution.py
+++ WeeklyChallenge/6주차_복서정렬하기/solution.py
@@ -4,17 +4,25 @@ def solution(weights, head2head):
     # N = none, W = Win, L = Lose
     for i in range(len(weights)):
         s = int(weights[i]) * 0.001
-
+        play = 0
+        # print(s)
         for j in range(len(head2head[i])):
+            if head2head[i][j] != "N":
+                play += 1
+
             if head2head[i][j] == "W":
                 if weights[j] > weights[i]:
                     s = s + 1001
                 else:
                     s = s + 1000
-        score.append(s)
+        if play != 0:
+            score.append(s / (play * 10000))
+        else:
+            score.append(s)
 
     sorted_score = sorted(score, reverse=True)
-
+    print(score)
+    print(sorted_score)
     for sc in sorted_score:
         answer.append(score.index(sc) + 1)
         score[answer[-1] - 1] = -1
