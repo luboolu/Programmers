a03ad9ce2a5a4c7f15e6f0011156115744154a0b
luboolu <kjjy98@naver.com>
2021-09-06 22:58:52 +0900

    210906

--- WeeklyChallenge/6주차_복서정렬하기/solution.py
+++ WeeklyChallenge/6주차_복서정렬하기/solution.py
@@ -1,30 +1,37 @@
 def solution(weights, head2head):
     answer = []
     score = []
+    BigWin = []
+    result = []
     # N = none, W = Win, L = Lose
     for i in range(len(weights)):
-        s = int(weights[i])
+        s = 0
         play = 0
-        # print(s)
+        bigwin = 0
         for j in range(len(head2head[i])):
             if head2head[i][j] != "N":
                 play += 1000000
 
             if head2head[i][j] == "W":
                 if weights[j] > weights[i]:
-                    s = s + 1001000
+                    s += 1000000
+                    bigwin += 1
                 else:
-                    s = s + 1000000
+                    s += 1000000
+
+        BigWin.append(bigwin)
         if play != 0:
             score.append((s / play) * 100)
         else:
             score.append(s)
 
-    sorted_score = sorted(score, reverse=True)
-    print(score)
-    print(sorted_score)
-    for sc in sorted_score:
-        answer.append(score.index(sc) + 1)
-        score[answer[-1] - 1] = -1
+        result.append({"i": i + 1, "weight": weights[i], "score": score[i], "bigwin": bigwin})
+
+    result.sort(key=lambda x: x["weight"], reverse=True)
+    result.sort(key=lambda x: x["bigwin"], reverse=True)
+    result.sort(key=lambda x: x["score"], reverse=True)
+
+    for i in result:
+        answer.append(i['i'])
 
     return answer
