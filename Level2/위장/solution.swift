912a51d785299b619642deae8680938f4a1595bc
luboolu <kjjy98@naver.com>
2022-03-22 23:22:43 +0900

    220322

--- Level2/위장/solution.swift
+++ Level2/위장/solution.swift
@@ -2,29 +2,20 @@ import Foundation
 
 func solution(_ clothes:[[String]]) -> Int {
 
-    var answer: Int = 0
-    var items: [String] = []
+    var answer: Int = 1
     var closet: [String : Int] = [:]
 
     for c in clothes {
-        print(c)
         if closet[c[1]] == nil {
             closet[c[1]] = 1
-            items.append(c[1])
         } else {
             closet[c[1]] = closet[c[1]]! + 1
         }
     }
 
-    //옷을 하나만 입는 경우
-    if closet.count == 1 {
-        return closet[items[0]] ?? 0
-    } else {
-        answer += closet.count
+    for c in closet {
+        answer *= (c.value + 1)
     }
 
-    //옷을 두개 이상 입는 경우
-
-
-    return answer
+    return answer - 1
 }
